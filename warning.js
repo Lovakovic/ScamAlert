00a400f86552ab5d44d66323e532bf46b2c9e84f
Lovakovic <lovakovdario@gmail.com>
2023-08-05 12:36:03 +0200

    Create a warning page and replace the notification for alerting the user of a malicious page

---
+++ warning.js
@@ -0,0 +1,14 @@
+async function init() {
+    let domain = new URLSearchParams(window.location.search).get('domain');
+    let stats = await browser.storage.local.get(domain);
+
+    document.getElementById('domain-name').textContent = domain;
+    document.getElementById('total-engines').textContent = stats[domain].malicious + stats[domain].undetected;
+    document.getElementById('malicious-engines').textContent = stats[domain].malicious;
+
+    document.getElementById('close-button').addEventListener('click', () => window.close());
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    init().catch(err => console.error(err));
+});
