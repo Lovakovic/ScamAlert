8b7d38f19fbe99dfc03439eb2af4d06f73fe9e5d
Lovakovic <lovakovdario@gmail.com>
2023-08-05 19:18:42 +0200

    Fix the calculation of total engines which scanned the domain

--- warn/warning.js
+++ warn/warning.js
@@ -3,7 +3,8 @@ async function init() {
     let stats = await browser.storage.local.get(domain);
 
     document.getElementById('domain-name').textContent = domain;
-    document.getElementById('total-engines').textContent = stats[domain].malicious + stats[domain].undetected;
+    document.getElementById('total-engines').textContent = stats[domain].harmless + stats[domain].malicious +
+        stats[domain].suspicious + stats[domain].undetected;
     document.getElementById('malicious-engines').textContent = stats[domain].malicious;
 
     document.getElementById('close-button').addEventListener('click', () => window.close());
