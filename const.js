5fbc691254ab6e6c7b2bf9a2a85b655d4791e226
Lovakovic <lovakovdario@gmail.com>
2023-08-06 12:35:00 +0200

    Implement a re-notification strategy for malicious domains whose results are already stored to local storage

--- const.js
+++ const.js
@@ -1,6 +1,6 @@
 // Timeouts for saving API calls
 export const POST_URL_TIMEOUT_MS = 10 * 1000;
-export const REPORT_FETCH_DELAY_MS = 40 * 1000;
+export const REPORT_FETCH_DELAY_MS = 20 * 1000;
 
 // Removal of stale scan results
 export const SCAN_EXPIRY_DURATION = 30 * 24 * 60 * 60 * 1000;  // 30 days
